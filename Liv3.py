75c8b4374397a10eb5bb63754bdb83ed4f0da152
WilliamSoucy <115368614+WilliamSoucy@users.noreply.github.com>
2023-04-03 12:06:31 -0400

    Update Liv3.py
    J'ai changer pas grand choses

--- Liv3.py
+++ Liv3.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
 from scipy.optimize import fsolve
 
+########################
+
 def Cp_ethanol(T):
     Cp=231+(-2.381)*T+(0.0133317)*T**2-(3.2*10**-5)*T**3+(3.15*10**-8)*T**4
     return Cp
@@ -18,23 +20,38 @@ def antoine(T):
 
 
 #Constantes 
-rho=0.8 #kg/L
-T_in=110 #°C
+rho = 0.8 #kg/L
+T_in = 110 #°C
 r = 1
 D = 2*r
 L = 5
-h = 
-rho = 
+kf = 317.33
+kp = 0.00057
 
 #Fonctions
-thetadvdh = [L, r]
+
 def dvdh(L, r, h) :
+    thetadvdh = [L, r]
     dvdh = (L*r**2)/(r*np.sqrt(1-((r-h)/r)**2)) + L*((-2*h**2+4*h*r-r**2)/np.sqrt(-h**2+2*h*r))
     return dvdh
 
-thetadhdh = [L, r, h, rho]
 def dhdt(L, r, h, rho, Fin, Fl, Fv) :
+    thetadhdh = [L, r, rho]
     Fl = kf * np.sqrt(h)
     dhdt = (1/(rho*dvdh(L, r, h)) * (Fin - Fl - Fv))
     return dhdt
+
+def dpdt() :
+    1/M 
+
+def Dtdt()
+
+
+
+
+
+
+
+
+
             
